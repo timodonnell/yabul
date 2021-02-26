e4aae01e799b7c073b282561c521bb8f13c19f9e
Tim O'Donnell <timodonnell@gmail.com>
2021-02-26 18:35:17 -0500

    remove setup.py and switch to setup.cfg

--- yabul/__init__.py
+++ yabul/__init__.py
@@ -1,6 +1,6 @@
 """
 Yet Another Bioinformatics Utility Library
 """
-from .version import __version__
+__version__ = "0.0.1"
 from .fasta import read_fasta, write_fasta
 from .align import align_pair
